576f4e171e08c526722d18a7ca7152c5b91edb83
zelalemzergaw <zola145103@gmail.com>
2023-07-26 18:22:29 -0500

    all service added in the same project

---
+++ microservice-new/order-service/src/main/java/com/mysecondservice/orderservice/model/Order.java
@@ -0,0 +1,24 @@
+package com.mysecondservice.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade=CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsLit;
+
+}
